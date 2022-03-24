dfbd0b77e1645267179dad851833378ec46f9694
Joanna-Golofit <sunderlajsa@gmail.com>
2022-03-24 11:26:59 +0100

    removed import of useState

--- src/components/hotels/Hotels.tsx
+++ src/components/hotels/Hotels.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Hotels.module.css";
 import { FaStar } from "react-icons/fa";
 import Rooms from "../rooms/Rooms";
